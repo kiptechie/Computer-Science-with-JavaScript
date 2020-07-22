59321ac112dcc14728bd63a1cfec53dd22ede42c
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-22 17:25:46 +0300

    Update quickSort.js

--- Sorting Algorithms/Quick Sort/quickSort.js
+++ Sorting Algorithms/Quick Sort/quickSort.js
@@ -8,19 +8,49 @@ Quick sort is then performed again on the left and right lists.
 */
 
 /*
-  Quicksort!
-  
-  Name your function quickSort.
-  
-  Quicksort should grab a pivot from the end and then separate the list (not including the pivot)
-  into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
-  lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
-  list, the pivot, and the "right" (or larger numbers) list and return that. The base case is when quickSort
-  is called on a list with length less-than-or-equal-to 1. In the base case, just return the array given.
-  
-  As always, you can change describe to xdescribe to prevent the unit tests from running while you're coding.
-  
-  No visualization is provided so feel free to use your own debugging methods (like console.log).
+[4,9,3,5] list
+-> 5 is made the pivot since it's the last in the array
+-> divide list into two lists, [4,3] and [9]
+-> call quicksort on those two lists
+
+[4, 3]
+-> 3 is pivot
+-> call quicksort on [] and [4]
+-> those both return as is as they are the base case of length 0 or 1
+-> concat [], 3, and [4]
+-> return [3,4]
+
+[9]
+-> returns as this it is a base case of length 1
+
+(back into the original function call)
+-> call concat on [3,4], 5, and [9]
+-> return [3,4,5,9]
+*/
+
+/*
+Another Big O of O(n log n) but takes up less memory than mergesort so it is often favored. 
+However it does really poorly if you pass it a sorted list. Think about it. 
+It would always have a pivot of the biggest number which defeats the effectiveness of the divide-and-conquer approach as one side will always contain all the elements. 
+Hence not good for lists you expect may already be sorted. 
+There are some tricks to employ to get around that like checking the beginning, middle, and end numbers and swapping them to try to get the best pivot, but that's outside our scope today. 
+There are a lot of subtle variants on quicksort.
+*/
+
+/*
+Quicksort!
+
+Name your function quickSort.
+
+Quicksort should grab a pivot from the end and then separate the list (not including the pivot)
+into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
+lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
+list, the pivot, and the "right" (or larger numbers) list and return that. The base case is when quickSort
+is called on a list with length less-than-or-equal-to 1. In the base case, just return the array given.
+
+As always, you can change describe to xdescribe to prevent the unit tests from running while you're coding.
+
+No visualization is provided so feel free to use your own debugging methods (like console.log).
 */
 
 const quickSort = nums => {
