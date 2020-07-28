2d7c3556167ba9705430a8135595231572fad464
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-28 15:38:58 +0300

    Update reduceFunction.js

--- Functional Programming/reduceFunction.js
+++ Functional Programming/reduceFunction.js
@@ -21,34 +21,33 @@ list.reduce(function(accumulator, letter) {
 */
 
 /*
-  Reduce
-  
-  Good for taking a list and reducing it down to one value in a user defined way.
-  
-  Test 1
-  Name your function addTogether
-  Take in a list and return the result of that list added together
-  Do not use a loop
-  
-  Test 2
-  Name your function concatenateStringsWithSpaces
-  Take in a list, return that string with those strings concatenated together with spaces between them
-  Don't worry about leading or trailing whitespace
-  Do not use .join or loops
-  
-  Test 3
-  Name your function squaresAndSubtracts
-  Map over your list, square each value, and then subtract them in order (take index 0, subtract index 1, 
-  then index 2, etc.)
-  Do not use a loop
-  
-  Test 4
-  Name your function myReduce
-  Implement your own reduce
-  myReduce takes three parameters: the list being operated on, a function to apply the reduction, and 
-  seed value to start the reduce
-  You will need to use a loop
-  
+Reduce
+
+Good for taking a list and reducing it down to one value in a user defined way.
+
+Test 1
+Name your function addTogether
+Take in a list and return the result of that list added together
+Do not use a loop
+
+Test 2
+Name your function concatenateStringsWithSpaces
+Take in a list, return that string with those strings concatenated together with spaces between them
+Don't worry about leading or trailing whitespace
+Do not use .join or loops
+
+Test 3
+Name your function squaresAndSubtracts
+Map over your list, square each value, and then subtract them in order (take index 0, subtract index 1, 
+then index 2, etc.)
+Do not use a loop
+
+Test 4
+Name your function myReduce
+Implement your own reduce
+myReduce takes three parameters: the list being operated on, a function to apply the reduction, and 
+seed value to start the reduce
+You will need to use a loop 
 */
 
 const addTogether = list => {
