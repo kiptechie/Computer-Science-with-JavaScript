15ebe751588c925c07416e1aed2533a09f5112f9
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2020-07-29 12:38:38 +0300

    Binary Search

---
+++ Divide & Conquer/binarySearch.js
@@ -0,0 +1,9 @@
+// Search for a value in a sorted array by cutting the side of the search area in half.
+
+// TASK: Implement binary search.
+
+function binarySearch(list, item) {
+
+}
+
+binarySearch([2, 6, 7, 90, 103], 90);
